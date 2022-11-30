f8d51aa5e8a55d2b7b25e6b092f8dd26c23c1f76
Myles <mylearagay@gmail.com>
2022-11-30 18:48:32 +0000

    Finished the game!
    Finished a rock, paper, scissors game played via console. Next steps would be to use HTML and CSS to improve upon this.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ function getComputerChoice() {
     let randomNum = getRandomInt(3);
     //console.log(randomNum); <--- Used to debug
 
+    let computerChoice = "";
+
     if (randomNum == 0) {
         computerChoice = "ROCK";
     } else if (randomNum == 1) {
@@ -16,7 +18,7 @@ function getComputerChoice() {
     }
 
     console.log("The computer chose: " + computerChoice);
-    return(computerChoice);
+    return computerChoice;
 }
 
 // USER INPUT
@@ -24,36 +26,68 @@ function getUserChoice() {
     let userChoice = prompt("Choose rock, paper or scissors: ").toLocaleUpperCase();
 
     console.log("You have chosen: " + userChoice);
-    return(userChoice);
+    return userChoice;
 }
 
-// PLAY A SINGLE ROUND
-function playRound(computerChoice, userChoice) {
+// CHECK WINNER
+function checkWinner(computerChoice, userChoice) {
     // User chooses rock
-    if (userChoice == "ROCK" && computerChoice == "ROCK") {
-        console.log("It's a tie!");
-    } else if (userChoice == "ROCK" && computerChoice == "PAPER") {
-        console.log("Uh-oh, Computer won!");
-    } else if (userChoice == "ROCK" && computerChoice == "SCISSORS") {
-        console.log("Congrats, you won!");
-    // User chooses paper
-    } else if (userChoice == "PAPER" && computerChoice == "ROCK") {
-        console.log("Congrats, you won!");
-    } else if (userChoice == "PAPER" && computerChoice == "PAPER") {
-        console.log("It's a tie!")
-    } else if (userChoice == "PAPER" && computerChoice == "SCISSORS") {
-        console.log("Uh-oh, Computer won!");
-    // User chooses scissors
-    } else if (userChoice == "SCISSORS" && computerChoice == "ROCK") {
-        console.log("Uh-oh, Computer won!");
-    } else if (userChoice == "SCISSORS" && computerChoice == "PAPER") {
-        console.log("Congrats, you won!");
-    } else if (userChoice == "SCISSORS" && computerChoice == "SCISSORS") {
-        console.log("It's a tie!")
+    if (userChoice == computerChoice) {
+        return "Tie";
+    } else if (
+        (userChoice == "ROCK" && computerChoice == "SCISSORS") ||
+        (userChoice == "PAPER" && computerChoice == "ROCK") ||
+        (userChoice == "SCISSORS" && computerChoice == "PAPER")
+    ) {
+        return "Player";
+    } else {
+        return "Computer";
     }
 }
 
-// 5-ROUND GAME
-function game() {
-    
+// PLAY A ROUND
+function playRound(computerChoice, userChoice) {
+    let result = checkWinner(computerChoice, userChoice);
+
+    if (result == "Tie") {
+        return "It's a tie!";
+    } else if (result == "Player") {
+        return `You Win! ${userChoice} beats ${computerChoice}`;
+    } else {
+        return `You Lose! ${computerChoice} beats ${userChoice}`;
+    }
 }
+
+// PLAY A 5-ROUND GAME
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    console.log("Let's play Rock, Paper, Scissors!");
+
+    for (let i = 0; i < 5; i++) {
+        let userChoice = getUserChoice();
+        let computerChoice = getComputerChoice();
+
+        console.log(playRound(computerChoice, userChoice));
+        console.log("------------------------------------------")
+
+        if (checkWinner(computerChoice, userChoice) == "Player") {
+            playerScore++;
+        } else if (checkWinner(computerChoice, userChoice) == "Computer") {
+            computerScore++;
+        }
+    }
+
+    console.log("Game Over!")
+
+    if (playerScore > computerScore) {
+        console.log("The Player Won!");
+    } else if (playerScore < computerScore) {
+        console.log("The Computer Won!");
+    } else {
+        console.log("Tied game!");
+    }
+}
+
+game();
